4f59ee4981c31a438fed5ce24c307b154a0b969d
carlxiso <lavaredasnarciso@gmail.com>
2022-04-08 12:09:10 +0100

    Navigation throughout sections food

--- meals/lib/components/category_item.dart
+++ meals/lib/components/category_item.dart
@@ -1,6 +1,9 @@
+// import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import '../models/category.dart';
+import '../screens/categories_meals_screen.dart';
 
 /// Component CategoryItem vai receber uma categoria como parametro
 ///
@@ -13,28 +16,43 @@ class CategoryItem extends StatelessWidget {
   /// Construtor
   const CategoryItem(this.category);
 
+  /// Método para selecionar categorias a aprtir do navigator
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) {
+        return const CategoriesMealsScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleLarge,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        gradient: LinearGradient(
-          colors: [
-            /// A primeira cor vai ser a cor passada com uma opacidade
-            category.color.withOpacity(0.5),
+    ///Com o InkWell Widget temos um feedback que o user faz um click
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(5),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          gradient: LinearGradient(
+            colors: [
+              /// A primeira cor vai ser a cor passada com uma opacidade
+              category.color.withOpacity(0.5),
 
-            /// A segunda cor vai ser a cor passada solida
-            category.color,
-          ],
+              /// A segunda cor vai ser a cor passada solida
+              category.color,
+            ],
 
-          /// Onde vai começar o gradient
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+            /// Onde vai começar o gradient
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
