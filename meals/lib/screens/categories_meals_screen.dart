db72cf202a93182b4ce9085c91c9829fb6fa0728
carlxiso <lavaredasnarciso@gmail.com>
2022-04-11 16:31:07 +0100

    Adding Images to Component Meals

--- meals/lib/screens/categories_meals_screen.dart
+++ meals/lib/screens/categories_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../components/meal_item.dart';
 import '../models/category.dart';
 import '../data/dummy_data.dart';
 
@@ -28,7 +29,7 @@ class CategoriesMealsScreen extends StatelessWidget {
       body: ListView.builder(
           itemCount: categoryMeals.length,
           itemBuilder: (context, index) {
-            return Text(categoryMeals[index].title);
+            return MealItem(categoryMeals[index]);
           }),
     );
   }
