9c5d0a6e8bb1fb4ac9ec3b95307b399049365092
carlxiso <lavaredasnarciso@gmail.com>
2022-04-13 09:30:35 +0100

    Adding links to the Drawer Section

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'screens/tabs_screen.dart';
 
+import 'screens/settings_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 import 'utils/app_routes.dart';
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
         AppRoutes.HOME: (context) => const TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (context) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (context) => MealDetailScreen(),
+        AppRoutes.SETTINGS: (context) => SettingsScreen(),
       },
     );
   }
